2d87910925e1bc3326829956495c35e11cbc7010
dadmarkbashir-1 <dadmarkbashir@gmail.com>
2024-12-03 10:15:25 +0300

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -34,23 +34,23 @@ const temps = moment().format('HH:mm:ss');
 const date = moment().format('DD/MM/YYYY');
 
   let infoMsg =  `
-      *𝗣𝗢𝗣𝗞𝗜𝗗 𝗠𝗗 𝗜𝗡𝗙𝗢* 
+      *DADMARK 𝗠𝗗 𝗜𝗡𝗙𝗢* 
 ❒───────────────────❒
 *𝗚𝗜𝗧𝗛𝗨𝗕 𝗟𝗜𝗡𝗞*
-> https://github.com/Popkiddevs/NORMAL-BOT-MD
+> https://github.com/dadmarkbashir/NORMAL-BOT-MD
 
 *𝗠𝗬 𝗖𝗛𝗔𝗡𝗡𝗘𝗟*
 > https://whatsapp.com/channel/0029VadQrNI8KMqo79BiHr3l
 ⁠
 ╭───────────────────❒
 │⏰⁠⁠⁠⁠ *RAM* : ${format(os.totalmem() - os.freemem())}/${format(os.totalmem())}
-│⚽ *DEV1* : *𝗣𝗢𝗣𝗞𝗜𝗗⭕*
-│⏰ *DEV2* : *𝗣𝗢𝗣𝗞𝗜𝗗*
+│⚽ *DEV1* : *DADMARK⭕*
+│⏰ *DEV2* : *DADMARK*
 ⁠⁠⁠⁠╰───────────────────❒
   `;
     
 let menuMsg = `
-     *𝗣𝗢𝗣𝗞𝗜𝗗 𝗧𝗘𝗖𝗛*
+     *DADMARK 𝗧𝗘𝗖𝗛*
 
 ❒────────────────────❒`;
 
