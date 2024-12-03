aa92dc7244ffbe01d4f256386a5042febbdcba16
dadmarkbashir-1 <dadmarkbashir@gmail.com>
2024-12-03 19:24:05 +0300

    Update deploy.js

--- commandes/deploy.js
+++ commandes/deploy.js
@@ -41,26 +41,26 @@ Hello ${nomAuteurMessage},,
 💙First type the sc ,repo or script command and you will get alpha md repository 
 💙From there you are required to get your *Session id* but how,??..
          𝐇𝐎𝐖 𝐓𝐎 𝐆𝐄𝐓 𝐒𝐄𝐒𝐒𝐈𝐎𝐍 𝐈𝐃
-✞ Open this link 👉 https://popkid-sessions-generator-n5yf.onrender.com/ then tap on pair code.
+✞ Open this link 👉 https://dadmark-sessions-generator-n5yf.onrender.com/ then tap on pair code.
 ✞ Enter your whatsapp number with the country code e.g;  254111385747 then tap submit 
 ✞Popkid Md owner , *POPKID* ,will send you the code immediately.Copy the code and whatsapp will bring a notification 
 ✞Tap on the notification and paste the code that Popkid md owner sent you.
-✞ After a successful login Popkid Md owner 😂 POPKID will send you a *session id* .some will wonder where the hell is that session ,,,...It is just right at your inbox /dm {your own number for whatsappp} 😂
+✞ After a successful login Popkid Md owner 😂 DADMARK will send you a *session id* .some will wonder where the hell is that session ,,,...It is just right at your inbox /dm {your own number for whatsappp} 😂
 ✞Copy the Session id {the one with unreadable codes and long copy it and send it to your deployer or deploy
      𝐇𝐎𝐖 𝐓𝐎 𝐃𝐄𝐏𝐋𝐎𝐘❤️
-✔ Now check on Popkid repository in github fork and give a star to this repository before doing anything 🌟or else Popkid Md owner won't allow you to deploy his bot💀.
+✔ Now check on Popkid repository in github fork and give a star to this repository before doing anything 🌟or else Dadmark Md owner won't allow you to deploy his bot💀.
 ✔Tap on heroku deploy tab  given there. First thing you should do is getting your *Heroku Api Key* insert it to the required space .
 ✔Enter the valid *heroku app name* and again repeat it to the blank space asking for app name accordingly.Fill everything and press on the below tab Deploy
 ✔In some heroku apps the buld logs might not show but it will eventually deploy 
-✔Now click on this devs number and give popkid md owner credits https://wa.me/message/HOBJXXD3BXCKI1
+✔Now click on this devs number and give dadmark md owner credits https://wa.me/message/HOBJXXD3BXCKI1
 ╰────────────────────☆`;
 let menuMsg = `
-     *REGARDS POPKID💙*`;
+     *REGARDS DADMARK💙*`;
    var lien = mybotpic();
 
    if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *popkid*, déveloper Popkid Tech" , gifPlayback : true }, { quoted: ms });
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *dadmark*, déveloper dadmark Tech" , gifPlayback : true }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
