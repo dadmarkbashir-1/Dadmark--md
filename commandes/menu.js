099010a417a1fe4db236435556feba1e2af29da9
Popkid <popkidtechs@gmail.com>
2024-12-10 20:35:13 +0300

    ⭕POP KID⭕

--- commandes/menu.js
+++ commandes/menu.js
@@ -51,7 +51,7 @@ const date = moment().format('DD/MM/YYYY');
         menuMsg += `*╭────⚃* *${cat}* *⚃*`;
         for (const cmd of coms[cat]) {
             menuMsg += `  
-*┊👣* ${cmd}`;
+*┊💖* ${cmd}`;
         }
         menuMsg += `
 *╰═════════════⚃* \n`
